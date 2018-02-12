a6b4626d09d9cab9c6a0ba287441d6e3b7ef8d64
Bernard Bado <bado.bernard94@gmail.com>
2018-02-12 15:33:09 +0100

    adding form submit

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -4,25 +4,48 @@ import _ from 'lodash';
 class MealPlan extends Component {
   state = {
     meal: '',
-    meals: []
+    meals: [],
+    showAddMealMessage: false
   };
 
   renderMeals() {
     return _.map(this.state.meals, meal => <li>{meal}</li>);
   }
 
+  componentWillMount() {
+    this.setState({meals: ["Eggs with bacon", "Spaghetti carbonara", "Oatmeal"]});
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.meals.length < this.state.meals.length) {
+      this.setState({showAddMealMessage: true});
+      setTimeout(() => this.setState({showAddMealMessage: false}), 1000);
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const meal = this.state.meal;
+    this.setState({
+      meals: [...this.state.meals, meal],
+      meal: ""
+    });
+  }
+
   render() {
     return(
       <div>
         <h2>Today you should eat this</h2>
-        <input
-          onChange={e => this.setState({meal: e.target.value})}
-          value={this.state.meal}
-          type="text"
-        />
-        <button
-          onClick={() => this.setState({meals: [...this.state.meals, this.state.meal]})}>
-          Add meal</button>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            onChange={e => this.setState({meal: e.target.value})}
+            value={this.state.meal}
+            type="text"
+          />
+          <button
+            type="submit">Add meal</button>
+        </form>
+        {this.state.showAddMealMessage ? <p>New meal added</p> : null}
         <ul>
           {this.renderMeals()}
         </ul>
